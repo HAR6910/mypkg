8e218b6732f9cf918b44c551ee6cd21d61c899ec
HAR <har@hurusagishi.com>
2025-12-30 19:56:57 +0900

    fix

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -1,16 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env python3
 # SPDX-FileCopyrightText: 2025 Sho Harukawa
 # SPDX-License-Identifier: BSD-3-Clause
 
-dir=~
-[ "$1" != "" ] && dir="$1"
-
-cd $dir/ros2_ws
-colcon build
-source $dir/.bashrc
-
+import rclpy
 from mypkg.bowlingscore_node import BowlingScoreNode
 
+def setup_module(module):
+    if not rclpy.ok():
+        rclpy.init()
+
+def teardown_module(module):
+    if rclpy.ok():
+        rclpy.shutdown()
 
 def test_strike_score():
     node = BowlingScoreNode()
@@ -18,17 +19,14 @@ def test_strike_score():
     score, frame = node.calculate_score()
     assert score == 24 
 
-
 def test_spare_score():
     node = BowlingScoreNode()
     node.role = [5, 5, 3]
     score, frame = node.calculate_score()
     assert score == 16
 
-
 def test_normal_score():
     node = BowlingScoreNode()
     node.role = [3, 6]
     score, frame = node.calculate_score()
     assert score == 9
-
