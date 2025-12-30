00300ab718c0f61df34db4fb42a2b3553bdc4fb9
HAR <har@hurusagishi.com>
2025-12-30 16:58:18 +0900

    fix

--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2025 Sho Harukawa
+# SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
@@ -6,7 +8,27 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+from mypkg.bowlingscore_node import BowlingScoreNode
+
+
+def test_strike_score():
+    node = BowlingScoreNode()
+    node.role = [10, 3, 4]
+    score, frame = node.calculate_score()
+    assert score == 24 
+
+
+def test_spare_score():
+    node = BowlingScoreNode()
+    node.role = [5, 5, 3]
+    score, frame = node.calculate_score()
+    assert score == 16
+
+
+def test_normal_score():
+    node = BowlingScoreNode()
+    node.role = [3, 6]
+    score, frame = node.calculate_score()
+    assert score == 9
+
